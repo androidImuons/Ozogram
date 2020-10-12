a4774deb7ad9b3d77b8d6929ac1ae078442d6f44
Admin <rahul.vhayaskar@imuons.com>
2020-10-12 12:45:50 +0530

    Rahul Vh

--- app/src/main/java/com/example/ozogram/view/activity/SplashActivity.java
+++ app/src/main/java/com/example/ozogram/view/activity/SplashActivity.java
@@ -3,14 +3,38 @@ package com.example.ozogram.view.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.ozogram.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final long SPLASH_TIME_MS = 2000;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, SPLASH_TIME_MS);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
     }
 }
