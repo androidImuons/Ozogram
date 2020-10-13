942217514f0e06b5aa15b4c4fabc3eb3678f9d2d
Mayuri Dalal <70504963+MayuriDalal@users.noreply.github.com>
2020-10-13 19:24:25 +0530

    Commiting Profile Activity.

---
+++ app/src/main/java/com/example/ozogram/view/fragment/GalleryFragment.java
@@ -0,0 +1,38 @@
+package com.example.ozogram.view.fragment;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ozogram.R;
+
+public class GalleryFragment extends Fragment {
+
+    private GalleryViewModel mViewModel;
+
+    public static GalleryFragment newInstance() {
+        return new GalleryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gallery_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(GalleryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
